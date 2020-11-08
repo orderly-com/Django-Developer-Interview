cae5b5414078af4bc5a49461c55e6c061111642a
Disink <a07485748@gmail.com>
2020-11-08 12:31:25 +0800

    Add website dynamic load function

--- x_1/django_crawler/crawler/views.py
+++ x_1/django_crawler/crawler/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from django.views.decorators.http import require_http_methods
+
 from crawler.models import Commodity
 from crawler.models import Category
 
@@ -12,19 +14,29 @@ def home_page(request):
 
     return render(request, 'home.html', {'category_list': category_list})
 
+
 def commodity_api_page(request, category_id):
     response = HttpResponse(content_type='text/csv')
+    writer = csv.writer(response)
+    writer.writerow(['title', 'price', 'discount_type', 'url'])
 
-    #commodity_list = list(Commodity.objects.all()[1:100].values())
-    #commodity_list = list(Commodity.objects.all().values())
     commodity_list = list(Category.objects.filter(
                               category_id=category_id,
                               status=1
                           )[0].commoditys.all().values())
 
-    writer = csv.writer(response)
-    writer.writerow(['title', 'price', 'discount_type', 'url'])
+    if 'start' in request.GET and 'end' in request.GET:
+        commodity_num = len(commodity_list)
+        start = int(request.GET['start'])
+        end = int(request.GET['end'])
 
+        if end > commodity_num:
+            commodity_list = commodity_list[start:commodity_num]
+        elif start > commodity_num:
+            commodity_list = []
+        else:
+            commodity_list = commodity_list[start:end]
+ 
     for commodity in commodity_list:
         title = commodity['title']
         price = commodity['price']
